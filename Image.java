479539bbd72f39f3d6dfd8e5c46c6b309b237559
petreusandreea <petreusandreea@yahoo.com>
2018-10-31 19:12:52 +0200

    Update Image.java

--- Image.java
+++ Image.java
@@ -1,11 +1,39 @@
 
-public class Image {
-	
-	private String name;
 
-	public Image(String name) {
-		// TODO Auto-generated constructor stub
+import java.util.concurrent.TimeUnit;
+
+public class Image implements Element{
+	private String name;
+	public Image (String name) throws InterruptedException {
+		this.name = name;
+		TimeUnit.SECONDS.sleep(5);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	@Override
+	public void print(){
+		System.out.println("IMAGE");
+		System.out.println(name);
+	}
+	@Override
+	public void add(Element e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void remove(Element e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void getChild(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }

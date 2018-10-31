f7fdba6f126f1087b3fc864e1dc0ad952d93693d
petreusandreea <petreusandreea@yahoo.com>
2018-10-31 18:43:24 +0200

    Update Author.java

--- Author.java
+++ Author.java
@@ -7,7 +7,12 @@ public class Author {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 	}
-
+        public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 
 	
